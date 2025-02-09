ab234b4309639d0363b7a13c07cf142233c0f9a7
Evgeny Cherkashin <eugeneai@irnok.net>
2025-02-09 13:11:08 +0000

    adding feature coloring according to a value

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    server: {
+        // open: true,
+        port: 8998,
+        host: '0.0.0.0',
+        allowedHosts: ['iec.isclan.ru']
+    }
 })
