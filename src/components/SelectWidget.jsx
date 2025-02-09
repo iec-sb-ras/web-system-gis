d49a06113ed8432ef6fee0d5ac0bf4587d4e0f56
Evgeny Cherkashin <eugeneai@irnok.net>
2025-02-09 15:09:24 +0000

    Working state + 2 space formatting

--- src/components/SelectWidget.jsx
+++ src/components/SelectWidget.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
 const SelectWidget = ({ options, onChange }) => {
-    return (
-        <select onChange={(e) => onChange(e.target.value)}>
-            {options.map((option) => (
-                <option key={option.value} value={option.value}>
-                    {option.label}
-                </option>
-            ))}
-        </select>
-    );
+  return (
+    <select onChange={(e) => onChange(e.target.value)}>
+      {options.map((option) => (
+        <option key={option.value} value={option.value}>
+          {option.label}
+        </option>
+      ))}
+    </select>
+  );
 };
 
 export default SelectWidget;
