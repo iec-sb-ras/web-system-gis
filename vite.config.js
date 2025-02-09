a621622df3c903a51cb6951e5b35b9a0cc1fca78
Evgeny Cherkashin <eugeneai@irnok.net>
2025-02-09 13:27:29 +0000

    Added server setup

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // open: true,
+    port: 8998,
+    host: '0.0.0.0',
+    allowedHosts: ['iec.isclan.ru']
+  }
 })
